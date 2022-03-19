5ca8e0cf1970c0efcad0405704cfde084fcf698e
lcgoandete <lcgoandete@hotmail.com>
2022-03-19 15:19:57 -0300

    fix: disable JSX Scope

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import Product from "./pages/product/Product";
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/react-in-jsx-scope */
+
+import Product from './pages/product/Product';
 
 function App() {
   return (
