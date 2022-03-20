e8a934075aa2aec1811b3ee72df38327caa0bff9
lcgoandete <lcgoandete@hotmail.com>
2022-03-20 17:38:07 -0300

    fix: update endpoint

--- src/hooks/usePayment.js
+++ src/hooks/usePayment.js
@@ -5,8 +5,7 @@ export default function payment() {
     try {
       const response = await axios({
         method: 'POST',
-        url: 'http://localhost:5000/payment/',
-        // url: 'https://mp-pro-backend.herokuapp.com/payment/',
+        url: 'https://mp-pro-backend.herokuapp.com/payment/',
         data: { payer, product },
       });
       return response.data;
